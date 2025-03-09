ca60d949ff019cebfd3d41d4b47684ef46a993a2
kcw0956 <103244652+kcw0956@users.noreply.github.com>
2025-03-09 23:50:41 +0900

    Update Program.cs

--- 06/CalculatorUsingEnum/Program.cs
+++ 06/CalculatorUsingEnum/Program.cs
@@ -6,17 +6,17 @@ namespace CalculatorUsingEnum
     {
         static void Main(string[] args)
         {
-            Console.Write("num1: ");
+            Console.Write("num1: "); 
             string num1String = Console.ReadLine();
-            int num1 = int.Parse(num1String);
+            int num1 = int.Parse(num1String);ㅍ //string에서 int 형으로 받아온 숫자 변환
 
             Console.Write("num2: ");
             string num2String = Console.ReadLine();
-            int num2 = int.Parse(num2String);
+            int num2 = int.Parse(num2String); 
 
             Console.Write("operation (+, -, *, /, %): ");
             string operationString = Console.ReadLine();
-            char operationChar = char.Parse(operationString);
+            char operationChar = char.Parse(operationString); //string을 char 형으로 변환    
 
             EOperator operation = (EOperator)operationChar;
 
